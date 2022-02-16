6ccf608f4457d11c835fbd0b81f3f000b4fd44e6
Michael Ullrich <mullrichael@gmail.com>
2022-02-16 21:14:23 +0100

    fix test

--- src/test/scala/AccountTest.scala
+++ src/test/scala/AccountTest.scala
@@ -4,6 +4,6 @@ import org.specs2.mutable.Specification
 import org.vermell.scrad.Account
 class AccountTest extends Specification {
   "calculateInterest" should {
-    Account.calculateInterest(1) === 3
+    Account.calculateInterest(1) === 2
   }
 }
