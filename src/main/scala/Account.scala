5411f9055a1cb065328d29dff6c38b11086cfd17
Michael Ullrich <mullrichael@gmail.com>
2022-03-05 18:32:13 +0100

    fix test

--- src/main/scala/Account.scala
+++ src/main/scala/Account.scala
@@ -1,10 +1,13 @@
 package org.vermell.scrad
 
-object Account extends App {
-  while (true) {
-    Thread.sleep(1000)
-    println(s"Hello World ${new java.util.Date}")
+object Account {
+  def main(args: Array[String]) = {
+    while (true) {
+    Thread.sleep (1000)
+    println (s"Hello World ${new java.util.Date}")
+    }
   }
+
   def calculateInterest: BigDecimal => BigDecimal = { interest =>
     interest * 2
   }
